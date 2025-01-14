9107fd4c15c106d8b7aec66434d9e1b4ea86e9bb
victoriano <vctr.maldonado.martin@gmail.com>
2025-01-14 22:39:27 +0100

    Nuevo repositorio para los ejercicios de clase

---
+++ Programacion FOC/main.java
@@ -0,0 +1,5 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hola aqui subire los proyectos de las clases de programación");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Vamos a empezar a subir aqui mis proyectos de clase");
+    }
+
+}
